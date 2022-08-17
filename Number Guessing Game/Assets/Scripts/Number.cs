a53fd995b79ed6483d99a9bdad27ee185e477033
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2022-08-17 11:03:27 -0700

    Update Number

--- Number Guessing Game/Assets/Scripts/Number.cs
+++ Number Guessing Game/Assets/Scripts/Number.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Number : MonoBehaviour
-{
+{   
+    private int guess;
+    private int minValue = 1;
+    private int maxValue = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Welcome to our Number Guessing game!");
+        Debug.Log("Guess a Number between " + minValue + " and" + maxValue);
     }
 
     // Update is called once per frame
